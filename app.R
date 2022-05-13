c201f8d32721d0b25e1c9b7f974e75c8a644a324
Wei Feng Ma <weimasemail@gmail.com>
2022-05-13 14:35:48 -0400

    multi-thread allowed to 6

--- app.R
+++ app.R
@@ -76,7 +76,7 @@ library(reactable)
 # 
 # tell shiny to try to paralle compute
 # future::plan("multisession")
-addArchRThreads(threads = 4) 
+addArchRThreads(threads = 6) 
 
 
 #### READ GOOGLE SHEET ####
